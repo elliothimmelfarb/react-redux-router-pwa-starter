2353fabfee2e49b713480e755eb3d441d54e0cd6
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-08-14 19:01:27 -0600

    removes react-router-redux

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
 
 import welcome from './_Welcome/duck'
 
 export default combineReducers({
   welcome,
-  routing: routerReducer,
 })
