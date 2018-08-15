838e79350925fadfd255eb594e35499542fbf7fd
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-08-14 18:58:53 -0600

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
-import logo from './logo.svg';
 import './App.css';
 import Router from './Router'
 
@@ -11,7 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <Router />
